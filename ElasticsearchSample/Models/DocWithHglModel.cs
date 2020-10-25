eb498b83545ea6d94b0577e8697da7cedc843289
kawabata2018 <chuanduanku52@gmail.com>
2020-10-26 00:37:04 +0900

    Add highlighting

---
+++ ElasticsearchSample/Models/DocWithHglModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElasticsearchSample.Models
+{
+    public class DocWithHglModel
+    {
+        public string DocumentId { get; set; }
+        public string DocumentName { get; set; }
+        public string TextWithHgl { get; set; }
+    }
+}
